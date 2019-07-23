635d7ebe9d376e60eebe38560eb29eaf9dabc2c9
RMH <rory.hennessey@gmail.com>
2019-07-23 16:13:15 -0400

    work in progress

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,22 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, MAR):
+        #MAR: Memory Address Register, holds the memory address we're reading or writing
+        return self.ram[MAR]
+
+    def ram_write(self, MDR):
+        #MDR: Memory Data Register, holds the value to write or the value just read
         pass
 
     def load(self):
@@ -18,25 +29,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +58,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +72,34 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        running = True
+
+        while running:
+            IR = self.ram[self.pc]
+            op_a = self.ram_read(self.pc + 1)
+            op_b = self.ram_read(self.pc + 2)
+            if IR == LDI:
+                self.reg[op_a] = op_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[op_a])
+                self.pc += 2
+            elif IR == HLT:
+                running = False
+                sys.exit(1)
+
+
+"""
+program = [
+            # From print8.ls8
+            0b10000010, # LDI R0,8
+            0b00000000,
+            0b00001000,
+            0b01000111, # PRN R0
+            0b00000000,
+            0b00000001, # HLT
+        ]
+"""
